ee2a43aa044e9dc289a451ec29bfc3be7ee2a07d
LittleTuna27 <cameronfortuna3@gmail.com>
2024-09-06 14:35:30 -0700

    learning binary decimals and hexadecimals
    im learning to write digits in (binary decimals and hexadecimals)

--- GD12_1133_Lab1_Fortuna_Cameron/Program.cs
+++ GD12_1133_Lab1_Fortuna_Cameron/Program.cs
@@ -5,7 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Cameron Fortuna Enta 1133!");
+            Console.WriteLine("My name is Cameron Fortuna Enta 1133!");
+            Console.WriteLine("im going to break down the numbers 0,1,2,4,8,16,32,64,100,255 into groups of Decimal, Binary, Hexadecimal");
+            Console.WriteLine("0, 0, 0");
+            Console.WriteLine("1, 1, 1");
+            Console.WriteLine("2, 10, 2");
+            Console.WriteLine("4, 100, 4");
+            Console.WriteLine("8, 100, 6");
+            Console.WriteLine("16, 1000, 10");
+            Console.WriteLine("32, 10000, 20");
+            Console.WriteLine("64, 100000, 40");
+            Console.WriteLine("100, 1100100, 64");
+            Console.WriteLine("255, 11111111, ff");
+            Console.WriteLine("goodebye to all that have read my wonderfull numbers on Todays date 2024-09-06 ");
         }
     }
 }
