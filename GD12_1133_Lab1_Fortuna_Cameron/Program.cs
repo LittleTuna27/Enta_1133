f5b226e4a06ef68df8194e1dc0914346272a8885
LittleTuna27 <cameronfortuna3@gmail.com>
2024-09-06 12:11:59 -0700

    Exploring Visual Studio
    Basic project with an extra line

---
+++ GD12_1133_Lab1_Fortuna_Cameron/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD12_1133_Lab1_Fortuna_Cameron
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Cameron Fortuna Enta 1133!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Enta_1133
 Enta 1113 introduction to game programing
+HEHE 2024-09-06
