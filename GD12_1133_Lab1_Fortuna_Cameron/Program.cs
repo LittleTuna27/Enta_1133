bba3499693a83ea6b34dc8526cddfadafc865e4d
LittleTuna27 <cameronfortuna3@gmail.com>
2024-09-06 14:45:13 -0700

    learning how to use Decimals, hexadecimals and binary
    learning how to use Decimals, hexadecimals and binary

--- GD12_1133_Lab1_Fortuna_Cameron/Program.cs
+++ GD12_1133_Lab1_Fortuna_Cameron/Program.cs
@@ -6,18 +6,18 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("My name is Cameron Fortuna Enta 1133!");
-            Console.WriteLine("im going to break down the numbers 0,1,2,4,8,16,32,64,100,255 into groups of Decimal, Binary, Hexadecimal");
+            Console.WriteLine("Im going to break down the numbers 0,1,2,4,8,16,32,64,100,255 into groups of Decimal, Binary, Hexadecimal");
             Console.WriteLine("0, 0, 0");
             Console.WriteLine("1, 1, 1");
             Console.WriteLine("2, 10, 2");
             Console.WriteLine("4, 100, 4");
-            Console.WriteLine("8, 100, 6");
-            Console.WriteLine("16, 1000, 10");
-            Console.WriteLine("32, 10000, 20");
-            Console.WriteLine("64, 100000, 40");
+            Console.WriteLine("8, 1000, 8");
+            Console.WriteLine("16, 10000, 10");
+            Console.WriteLine("32, 100000, 20");
+            Console.WriteLine("64, 1000000, 40");
             Console.WriteLine("100, 1100100, 64");
             Console.WriteLine("255, 11111111, ff");
-            Console.WriteLine("goodebye to all that have read my wonderfull numbers on Todays date 2024-09-06 ");
+            Console.WriteLine("goodebye to all that have read my wonderfull numbers on Todays date 2024-09-06");
         }
     }
 }
