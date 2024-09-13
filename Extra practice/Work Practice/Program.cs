584ed6f238b234cf615157f35db0c7e2eb5cc592
LittleTuna27 <cameronfortuna3@gmail.com>
2024-09-13 10:42:08 -0700

    practiced with bineary decimal and hexadwecimal

---
+++ Extra practice/Work Practice/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Work_Practice
+namespace GD12_1133_Lab1_Fortuna_Cameron
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Cameron Fortuna Enta 1133!");
+            Console.WriteLine(" 48 65 6c 6c 6f 20 74 68 65 72 65 20 47 65 6e 65 72 61 6c 20 4b 65 6e 6f 62 69 21")
+            Console.WriteLine("Hello There General kenobi!");
+            
+        }
+    }
+}
---
+++ Extra practice/practice binary, decimals, hexadecimals/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace practice_binary__decimals__hexadecimals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Cameron Fortuna Enta 1133!");
+            Console.WriteLine("Im going to break down the numbers 48 65 6c 6c 6f 20 74 68 65 72 65 20 47 65 6e 65 72 61 6c 20 4b 65 6e 6f 62 69 21 into groups of Decimal, Binary, Hexadecimal");
+            Console.WriteLine("0, 0, 0");
+            Console.WriteLine("1, 1, 1");
+            Console.WriteLine("2, 10, 2");
+            Console.WriteLine("4, 100, 4");
+            Console.WriteLine("8, 1000, 8");
+            Console.WriteLine("16, 10000, 10");
+            Console.WriteLine("32, 100000, 20");
+            Console.WriteLine("64, 1000000, 40");
+            Console.WriteLine("100, 1100100, 64");
+            Console.WriteLine("255, 11111111, ff");
+            Console.WriteLine("goodebye to all that have read my wonderfull numbers on Todays date 2024-09-06");
+        }
+    }
+    }
+}
